26f8982f887c8891e3808235911036b65ef6ed88
Christoph Renzing <christoph.renzing@imf.tu-freiberg.de>
2025-07-04 10:20:34 +0200

    new fitting algorithm and final results

--- utilities/optimizations.py
+++ utilities/optimizations.py
@@ -1,8 +1,9 @@
 import numpy as np
 
-
-from .helpers import extract_wear_contours_from_measurement, calculate_area_between_contours, root_mean_square_value,calculate_wear_contour
+from .helpers import extract_wear_contours_from_measurement, calculate_area_between_contours, root_mean_square_value, \
+    calculate_wear_contour
 from .wear_coefficient_functions import get_wear_coefficient
+
 
 def solve_total_wear_depths_as_contour_line(params, sequences):
     area_errors = []
@@ -77,6 +78,30 @@ def solve_total_wear_depths_as_contour_line_6_pass_block(params, sequences):
                         continue
     rms_error = root_mean_square_value(np.array(area_errors))
     print("RMS error: ", rms_error)
+    return np.array(area_errors)
+
+
+def solve_total_wear_depths_as_contour_line_4_and_6_pass_block(params, sequences):
+    area_errors = []
+
+    for entry in sequences:
+        groove_label = entry["label"]
+        sequence = entry["sequence"]
+        roll_passes = sequence.roll_passes
+        measurements = extract_wear_contours_from_measurement(groove_label)
+
+        for measurement_id, labels, tonnages, measured_wear_contours in measurements:
+            for roll_pass in roll_passes:
+                for label, tonnage, measured_wear_contour in zip(labels, tonnages, measured_wear_contours):
+                    if roll_pass.label == label and label in ["17-H", "18-V", "21-H", "22-V", "23-H", "24-V"]:
+
+                        wear_coefficient = get_wear_coefficient(roll_pass, params)
+                        calculated_wear_contour = calculate_wear_contour(roll_pass, wear_coefficient, tonnage * 1000)
+                        area_error = calculate_area_between_contours(calculated_wear_contour, measured_wear_contour)
+                        area_errors.append(area_error)
+                    else:
+                        continue
+    rms_error = root_mean_square_value(np.array(area_errors))
     return np.array(area_errors)
 
 
